55f3d02ac5f84ee0242821b9804f3d8b599d7c6a
mwielgosz04 <mwielgosz04@gmail.com>
2020-04-04 17:04:59 +0200

    task2 implemented

--- task1.py
+++ task1.py
@@ -5,3 +5,19 @@ app = FastAPI()
 @app.get('/')
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.get('/method')
+def methodGet():
+    return {"method": "GET"}
+
+@app.post('/method')
+def methodPost():
+    return {"method": "POST"}
+
+@app.put('/method')
+def methodPut():
+    return {"method": "PUT"}
+
+@app.delete('/method')
+def methodDelete():
+    return {"method": "DELETE"}

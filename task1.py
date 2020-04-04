8c3127b6ab0cd79bb9ba69ccc3b67351e8a7a88f
mwielgosz04 <mwielgosz04@gmail.com>
2020-04-04 17:33:23 +0200

    task 3

--- task1.py
+++ task1.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
+currentNumber: int = 0
+
 app = FastAPI()
+
+class PatientRequest(BaseModel):
+    name: str
+    surname: str
+
+class PatientReponse(BaseModel):
+    id: int
+    patient: PatientRequest
 
 @app.get('/')
 def hello_world():
@@ -21,3 +33,10 @@ def methodPut():
 @app.delete('/method')
 def methodDelete():
     return {"method": "DELETE"}
+
+@app.post('/patient')
+def patientInfo(patient: PatientRequest):
+    global currentNumber
+    patientFullInfo = PatientReponse(id = currentNumber, patient = patient)
+    currentNumber += 1
+    return patientFullInfo
